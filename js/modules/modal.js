2be1b578d5c5cd915b70ede3add869d2b0a9190c
Lars Cortelini <lmcortelini@gmail.com>
2021-12-19 15:20:38 -0300

    modal refatorado com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,22 +1,47 @@
-export default function modal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class Modal {
+  constructor(openButton, closeButton, modalContainer) {
+    this.botaoAbrir = document.querySelector(openButton);
+    this.botaoFechar = document.querySelector(closeButton);
+    this.containerModal = document.querySelector(modalContainer);
+    this.activeClass = "ativo";
 
-  function toggleModal(event) {
-    event.preventDefault();
-    containerModal.classList.toggle("ativo");
+    //bind this ao callback para fazer referência ao objeto da classe
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clickForaModal = this.clickForaModal.bind(this);
   }
 
-  function fecharClickFora(event) {
-    if (event.target === this) {
-      toggleModal(event);
+  //abre e fecha o modal
+  toggleModal() {
+    this.containerModal.classList.toggle(this.activeClass);
+  }
+
+  //adiciona o evento de toggle ao modal
+  eventToggleModal(event) {
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  //fecha o modal ao clicar do lado de fora
+  clickForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toggleModal(event);
     }
   }
 
-  if (botaoAbrir && botaoFechar && containerModal) {
-    botaoAbrir.addEventListener("click", toggleModal);
-    botaoFechar.addEventListener("click", toggleModal);
-    containerModal.addEventListener("click", fecharClickFora);
+  //adiciona eventos aos elementos do modal
+  addModalEvent() {
+    this.botaoAbrir.addEventListener("click", this.eventToggleModal);
+    this.botaoFechar.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.clickForaModal);
+  }
+
+  //quando a seleção for com query selector, ele retorna um valor falsy, sendo desnecessário
+  // adicionar o length. Diferente do query selector all, que devolve uma node list
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addModalEvent();
+    }
+
+    return this;
   }
 }
