4cf6b2f364960be2247948a4f97e00ba7c1002ca
Lars Cortelini <lmcortelini@gmail.com>
2021-12-21 20:20:10 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,13 +1,12 @@
-export default function initFetchBitcoin() {
-  fetch("https://blockchain.info/ticker")
+export default function fetchBitcoin(url, target) {
+  fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      const btcPreco = document.querySelector(".btc-preco");
+      const btcPreco = document.querySelector(target);
 
       btcPreco.innerText = (100 / json.BRL.sell).toFixed(6);
-      console.log(json.BRL.sell);
     })
     .catch((erro) => {
-      console.log(Error(erro));
+      console.log(erro);
     });
 }
